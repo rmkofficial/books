5ea2d8a6ed5b2e4e3ffd962d25d5be3c670a08c8
Ramazan Mert Kaya <kmert0@outlook.com>
2023-05-09 22:48:20 +0300

    Toggling Form Values

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,16 +1,37 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
 function BookShow({ book, onDelete }) {
-  const handleClick = () => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
     onDelete(book.id);
   };
 
-  return <div className="book-show">
-    {book.title}
-    <div className="actions">
-      <button className="delete" onClick={handleClick}>
-        Delete
-      </button>
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  let content = <h3>{book.title}</h3>
+  if (showEdit) {
+    content = <BookEdit />
+  } else {
+
+  }
+
+  return (
+    <div className="book-show">
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
     </div>
-    </div>;
+  );
 }
 
 export default BookShow;
