4532463aadcb400332d1255fdd8e4ef1d9771d07
Ramazan Mert Kaya <kmert0@outlook.com>
2023-05-09 22:37:46 +0300

    Deleting Records

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
@@ -16,7 +23,7 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
